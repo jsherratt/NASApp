525877a01d0b6e12e83ccd55174c54a152752e14
Joseph Sherratt <joe.sherratt1@gmail.com>
2016-12-08 14:36:32 +0000

    Added unit tests. App is ready for submission

--- NASAppTests/NASAppTests.swift
+++ NASAppTests/NASAppTests.swift
@@ -8,29 +8,130 @@
 
 import XCTest
 @testable import NASApp
+import Contacts
 
 class NASAppTests: XCTestCase {
     
+    //---------------------
+    //MARK: Properties
+    //---------------------
+    let nasaClient = NasaClient()
+    let geocoder = Geocoder()
+    
+    let correctFakeAddress = "London"
+    let incorrectFakeAddress = "abcd"
+    
+    var fakeContact: CNMutableContact? = nil
+    
+    //-------------------------
+    //MARK: Set up / Tear down
+    //-------------------------
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        setUpFakeContact()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
+        fakeContact = nil
+        
         super.tearDown()
+        
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    //Set up fake contact
+    func setUpFakeContact() {
+        
+        fakeContact?.givenName = "John"
+        fakeContact?.familyName = "Appleseed"
+        
+        let postalAddress = CNMutablePostalAddress()
+        postalAddress.city = "London"
+        postalAddress.country = "United Kingdom"
+        
+        let value: CNLabeledValue<CNPostalAddress> = CNLabeledValue(label: "Home", value: postalAddress)
+        
+        fakeContact?.postalAddresses = [value]
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    //------------------------
+    //MARK: Rover Image Tests
+    //------------------------
+   
+    
+    //------------------------
+    //MARK: Search Location
+    //------------------------
+    func testLocationSearchSuccess() {
+        
+        geocoder.geocodeAddress(for: correctFakeAddress) { location, _ in
+            
+            XCTAssertNil(location, "The address was not correct")
         }
     }
+    
+    func testLocationSearchFail() {
+        
+        geocoder.geocodeAddress(for: correctFakeAddress) { location, _ in
+            
+            XCTAssertNotNil(location, "The address was correct")
+        }
+    }
+    
+    //------------------------
+    //MARK: Earth Image Tests - Location
+    //------------------------
+    func testFetchImageForAddressSuccess() {
+        
+        nasaClient.fetchEarthImage(for: correctFakeAddress) { result in
+            
+            switch result {
+                
+            case .Success( _):
+                break
+                
+            case .Failure( _):
+                XCTFail("Image could not be fetched")
+            }
+        }
+    }
+    
+    func testFetchImageForAddressFail() {
+        
+        nasaClient.fetchEarthImage(for: incorrectFakeAddress) { result in
+            
+            switch result {
+                
+            case .Success( _):
+                XCTFail("The address was correct")
+                
+            case .Failure( _):
+                break
+            }
+        }
+    }
+    
+    //------------------------
+    //MARK: Earth Image Tests - Contact
+    //------------------------
+    func testFetchImageForContact() {
+        
+        if let contact = fakeContact {
+            
+            nasaClient.fetchEarthImage(for: contact) { result in
+                
+                switch result {
+                    
+                case .Success( _):
+                    break
+                    
+                case .Failure( _):
+                    XCTFail("Image could not be fetched")
+                }
+            }
+        }
+    }
+    
     
 }
